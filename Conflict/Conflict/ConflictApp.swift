93d9e1a69a7e0e14be68fb37badaa9947495ec52
KhuePM <minhkhuepham99@gmail.com>
2025-06-13 23:36:02 +0700

    init project

---
+++ Conflict/Conflict/ConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConflictApp.swift
+//  Conflict
+//
+//  Created by KhuePM on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
